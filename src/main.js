9ed44736033d68f8575b185dd3d8ef42db2fbded
Junyuan Wu <97990879+2050820354@users.noreply.github.com>
2023-10-08 10:39:48 +0800

    Add files via upload

--- src/main.js
+++ src/main.js
@@ -1,5 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
+import './styles/base.css'
+import './styles/reset.css'
+import './assets/iconfont/iconfont.css'
+
+import BootstrapVue from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+Vue.use(BootstrapVue)
 
 Vue.config.productionTip = false
 
