2fc489529d3cf6b882186f60b138cc8f16d7f8d7
Junyuan Wu <97990879+2050820354@users.noreply.github.com>
2023-10-08 10:38:38 +0800

    Add files via upload

---
+++ App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <jtChat></jtChat>
+  </div>
+</template>
+
+<script>
+import jtChat from './components/jtChat.vue'
+export default {
+  components: {
+    jtChat
+  }
+}
+</script>
+
+<style>
+
+</style>
